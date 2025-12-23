aec96b5a413394d5775eecefab9fe21c574cd5e4
michbat <michael.batn@hotmail.com>
2025-12-23 02:58:09 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+// Import Bootstrap CSS and JS
+import * as bootstrap from 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// Import custom styles
+import './style.css'
+
+// Votre code JavaScript pour l'interactivité ici
+console.log('Bootstrap version:', bootstrap.Modal.VERSION)
+
+// Exemple : ajouter des événements
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Page chargée et Bootstrap prêt !')
+
+  // Vous pouvez ajouter votre logique JavaScript ici
+})
